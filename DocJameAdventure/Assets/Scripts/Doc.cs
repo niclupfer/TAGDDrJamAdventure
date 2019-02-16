8f486ced02241a244ede52ab828ed281cc243cd8
Nic Lupfer <nlupfer@gmail.com>
2019-02-16 14:31:46 -0600

    guy moving

---
+++ DocJameAdventure/Assets/Scripts/Doc.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Doc : MonoBehaviour
+{
+	public float speed;
+
+	Rigidbody rb;
+
+    void Start()
+    {
+		rb = GetComponent<Rigidbody> ();
+
+    }
+
+    void Update()
+    {
+		var vert = Input.GetAxis ("Vertical");
+		var horz = Input.GetAxis ("Horizontal");
+
+		var posDelta = (new Vector3 (horz, 0, vert)).normalized * speed * Time.deltaTime;
+
+		//transform.position += posDelta;
+		rb.MovePosition(transform.position + posDelta);
+
+		if (Input.GetKeyDown (KeyCode.E))
+			Whatever ();
+    }
+
+	void Whatever()
+	{
+		GetComponentInChildren<MeshRenderer> ().material.color = Color.red;
+	}
+}
---
+++ DocJameAdventure/Assets/Scripts/FollowCam.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    public GameObject target;
+	public float smoothing;
+
+
+	void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
