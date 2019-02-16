680c59edd15dcbce5da78baa18105445cfae225e
Nic Lupfer <nlupfer@gmail.com>
2019-02-16 14:55:46 -0600

    turning and stuff

--- DocJameAdventure/Assets/Scripts/Doc.cs
+++ DocJameAdventure/Assets/Scripts/Doc.cs
@@ -7,30 +7,49 @@ public class Doc : MonoBehaviour
 {
 	public float speed;
 
+	public GameObject visual;
+
 	Rigidbody rb;
+
+	float goalAngle;
+	float currentAngle;
+	public float turnSmoothing;
 
     void Start()
     {
 		rb = GetComponent<Rigidbody> ();
-
+		currentAngle = 0;
+		goalAngle = 0;
     }
 
-    void Update()
-    {
+    void FixedUpdate()
+	{
 		var vert = Input.GetAxis ("Vertical");
 		var horz = Input.GetAxis ("Horizontal");
 
-		var posDelta = (new Vector3 (horz, 0, vert)).normalized * speed * Time.deltaTime;
+		var vec = (new Vector3 (horz, 0, vert)).normalized;
+
+		var posDelta = vec * speed * Time.deltaTime;
 
 		//transform.position += posDelta;
-		rb.MovePosition(transform.position + posDelta);
+		rb.MovePosition (transform.position + posDelta);
 
+		if (vec.magnitude > 0)
+		{
+			goalAngle = Mathf.Atan2 (horz, vert) * Mathf.Rad2Deg;
+			currentAngle = Mathf.Lerp (currentAngle, goalAngle, turnSmoothing * Time.deltaTime);
+			visual.transform.localEulerAngles = new Vector3 (0, currentAngle, 0);
+		}
+
+	}
+
+	void Update() {
 		if (Input.GetKeyDown (KeyCode.E))
 			Whatever ();
     }
 
 	void Whatever()
 	{
-		GetComponentInChildren<MeshRenderer> ().material.color = Color.red;
+		//GetComponentsInChildren<MeshRenderer> ().material.color = Color.red;
 	}
 }
--- DocJameAdventure/Assets/Scripts/FollowCam.cs
+++ DocJameAdventure/Assets/Scripts/FollowCam.cs
@@ -7,14 +7,19 @@ public class FollowCam : MonoBehaviour
     public GameObject target;
 	public float smoothing;
 
-
 	void Start()
     {
         
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        
+		if (target != null) {
+			var pos = target.transform.position;
+
+			transform.position =
+				Vector3.Lerp (transform.position,
+					pos, smoothing * Time.deltaTime);
+		}
     }
 }
