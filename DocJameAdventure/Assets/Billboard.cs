85d13fea436a7fa8ceefb2071aa2f8e4100afa0a
Nic Lupfer <nlupfer@gmail.com>
2019-02-16 15:42:13 -0600

    forest spawning

---
+++ DocJameAdventure/Assets/Billboard.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+	Camera cam;
+
+    void Start()
+    {
+		cam = Camera.main;
+    }
+
+    void Update()
+    {
+		var flatPos = cam.transform.position;
+		flatPos.y = 0;
+
+		var myFlatPos = new Vector3(transform.position.x, 0, transform.position.z);
+
+		var angle = Vector3.Angle (flatPos, myFlatPos);
+		transform.eulerAngles = new Vector3 (0, angle, 0);
+
+		//transform.LookAt (flatPos);
+    }
+}
---
+++ DocJameAdventure/Assets/Forest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Forest : MonoBehaviour
+{
+	public GameObject[] objs;
+
+	public float forestWidth;
+	public float cellSize;
+	public float spawnChance;
+	
+    void Start()
+	{
+		BuildForest ();
+	}
+
+
+
+	void BuildForest()
+    {
+		var pos = new Vector3(-forestWidth * 0.5f, 0, -forestWidth * 0.5f);
+
+		int rows = Mathf.FloorToInt (forestWidth / cellSize);
+		for(int x = 0; x < rows; x++)
+		{
+			for (int y = 0; y < rows; y++) {
+				var n = Random.value;
+				if (n < spawnChance) {
+
+					int o = Mathf.FloorToInt(Random.Range (0, objs.Length));
+					var newObj = Instantiate (objs [o]);
+					newObj.transform.parent = transform;
+					newObj.transform.localPosition = pos + new Vector3 (x * cellSize, 0, y * cellSize);					
+				}
+			}
+		}
+    }
+}
